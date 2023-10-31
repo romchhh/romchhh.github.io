992b29d204f335aa608cc7ab9471e67fdb38952d
kxtzzl <123520267+romchhh@users.noreply.github.com>
2023-10-31 17:56:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,14 +21,107 @@ function getApplications() {
             guests: "Гості 2",
             time: "Час 2",
             status: "Очікує підтвердження" // Додайте поле статусу
+        },
+        {
+            name: "Ім'я 2",
+            chat_id: "ID 2",
+            username: "Юзернейм 2",
+            adress: "Вулиця 2, Будинок 2, Квартира 2",
+            car: "Машина 2",
+            guests: "Гості 2",
+            time: "Час 2",
+            status: "Очікує підтвердження" // Додайте поле статусу
+        },
+        {
+            name: "Ім'я 2",
+            chat_id: "ID 2",
+            username: "Юзернейм 2",
+            adress: "Вулиця 2, Будинок 2, Квартира 2",
+            car: "Машина 2",
+            guests: "Гості 2",
+            time: "Час 2",
+            status: "Очікує підтвердження" // Додайте поле статусу
+        },
+        {
+            name: "Ім'я 2",
+            chat_id: "ID 2",
+            username: "Юзернейм 2",
+            adress: "Вулиця 2, Будинок 2, Квартира 2",
+            car: "Машина 2",
+            guests: "Гості 2",
+            time: "Час 2",
+            status: "Очікує підтвердження" // Додайте поле статусу
+        },
+        {
+            name: "Ім'я 2",
+            chat_id: "ID 2",
+            username: "Юзернейм 2",
+            adress: "Вулиця 2, Будинок 2, Квартира 2",
+            car: "Машина 2",
+            guests: "Гості 2",
+            time: "Час 2",
+            status: "Очікує підтвердження" // Додайте поле статусу
+        },
+        {
+            name: "Ім'я 2",
+            chat_id: "ID 2",
+            username: "Юзернейм 2",
+            adress: "Вулиця 2, Будинок 2, Квартира 2",
+            car: "Машина 2",
+            guests: "Гості 2",
+            time: "Час 2",
+            status: "Очікує підтвердження" // Додайте поле статусу
+        }, {
+            name: "Ім'я 2",
+            chat_id: "ID 2",
+            username: "Юзернейм 2",
+            adress: "Вулиця 2, Будинок 2, Квартира 2",
+            car: "Машина 2",
+            guests: "Гості 2",
+            time: "Час 2",
+            status: "Очікує підтвердження" // Додайте поле статусу
+        },
+        {
+            name: "Ім'я 2",
+            chat_id: "ID 2",
+            username: "Юзернейм 2",
+            adress: "Вулиця 2, Будинок 2, Квартира 2",
+            car: "Машина 2",
+            guests: "Гості 2",
+            time: "Час 2",
+            status: "Очікує підтвердження" // Додайте поле статусу
         }
+
+
+
+
     ];
 }
 
-// Функція для відображення заявок в таблиці
+
+
+function sortApplications() {
+    var applications = getApplications();
+
+    applications.sort(function(a, b) {
+        if (a.status === 'Очікує підтвердження' && b.status !== 'Очікує підтвердження') {
+            return -1;
+        } else if (a.status !== 'Очікує підтвердження' && b.status === 'Очікує підтвердження') {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    return applications;
+}
+
 function displayApplications() {
     var tableBody = document.querySelector("#applicationTable tbody");
-    var applications = getApplications();
+    var applications = sortApplications();
+
+    // Очищаємо таблицю перед оновленням
+    tableBody.innerHTML = '';
 
     applications.forEach(function(application, index) {
         var row = tableBody.insertRow();
@@ -46,35 +139,45 @@ function displayApplications() {
         var actionsCell = row.insertCell();
         var confirmButton = document.createElement("button");
         confirmButton.textContent = "Підтвердити";
-        confirmButton.classList.add("confirm-button"); // Додали клас для стилю
+        confirmButton.classList.add("confirm-button");
         confirmButton.addEventListener("click", function() {
             if (!confirmButton.disabled) {
                 // Заблокувати кнопку після натискання
                 confirmButton.disabled = true;
                 rejectButton.disabled = true;
                 confirmAction("підтвердити", row, application);
+                // Змінюємо стиль після натискання
+                row.style.fontWeight = 'normal';
+                row.style.cursor = 'default';
             }
         });
 
         var rejectButton = document.createElement("button");
         rejectButton.textContent = "Відхилити";
-        rejectButton.classList.add("reject-button"); // Додали клас для стилю
+        rejectButton.classList.add("reject-button");
         rejectButton.addEventListener("click", function() {
             if (!rejectButton.disabled) {
                 // Заблокувати кнопку після натискання
                 confirmButton.disabled = true;
                 rejectButton.disabled = true;
                 confirmAction("відхилити", row, application);
+                // Змінюємо стиль після натискання
+                row.style.fontWeight = 'normal';
+                row.style.cursor = 'default';
             }
         });
 
         actionsCell.appendChild(confirmButton);
         actionsCell.appendChild(rejectButton);
+
+        // Перевірка статусу і застосування стилю
+        if (application.status === 'Очікує підтвердження') {
+            row.style.fontWeight = 'bold';
+            row.style.cursor = 'pointer';
+        }
     });
 }
 
-
-// Функція для відображення підтвердження перед відхиленням або підтвердженням
 function confirmAction(action, row, application) {
     if (confirm(`Ви впевнені, що бажаєте ${action} заявку?`)) {
         // Змінюємо статус заявки
@@ -83,9 +186,14 @@ function confirmAction(action, row, application) {
         // Оновлюємо текст статусу в таблиці
         var statusCell = row.cells[row.cells.length - 2];
         statusCell.textContent = application.status;
+    } else {
+        // Якщо користувач відмінив, то розблоковуємо кнопки
+        var confirmButton = row.querySelector(".confirm-button");
+        var rejectButton = row.querySelector(".reject-button");
+        confirmButton.disabled = false;
+        rejectButton.disabled = false;
     }
 }
 
 
-// Викликати функцію для відображення заявок при завантаженні сторінки
 displayApplications();
